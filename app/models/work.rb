956d51e84803e40ed09adfd1488095f2bc77b679
Catherina <catherine.pchelina@gmail.com>
2020-05-14 15:15:36 -0700

    Sort works by books, movies and albums

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,15 @@
 class Work < ApplicationRecord
+
+  def self.sort_album_works
+    return Work.where(category: "album")
+  end
+
+  def self.sort_book_works
+    return Work.where(category: "book")
+  end
+
+  def self.sort_movie_works
+    return Work.where(category: "movie")
+  end
+
 end
