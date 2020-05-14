23d7f94ed2b8743637024312ba80bf9e2dd9e2f1
Catherina <catherine.pchelina@gmail.com>
2020-05-14 12:50:39 -0700

    Add create action and strong params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -16,4 +16,24 @@ class WorksController < ApplicationController
     @work = Work.new
   end
 
+  def create 
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+      flash[:success] = "Successfully created #{@work.category} #{@work.id}"
+      return
+    else
+      render :new
+      return
+    end
+
+  end
+
+  private 
+
+  def work_params 
+    return params.require(:work).permit(:category, :title, :creator,
+    :publication_year, :description)
+  end
+
 end
