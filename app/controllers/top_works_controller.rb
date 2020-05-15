d9bb668311357826f26c5918cd95411bbc5cdd06
Catherina <catherine.pchelina@gmail.com>
2020-05-14 17:55:42 -0700

    Add top_works as a home page

---
+++ app/controllers/top_works_controller.rb
@@ -0,0 +1,5 @@
+class TopWorksController < ApplicationController
+
+  def index 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get "top_works/index"
+
+  root to: 'top_works#index'
+
   resources :works
 end
