72f8c69233d1faaad79e166fff16974b0f855cfe
Catherina <catherine.pchelina@gmail.com>
2020-05-17 14:20:31 -0700

    Add Vote model, generate migrations to relate work and user with votes

---
+++ app/models/vote.rb
@@ -0,0 +1,4 @@
+class Vote < ApplicationRecord
+  belongs_to :work
+  belongs-to :user
+end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ApplicationRecord
+  has_many :votes
 
   validates :category, presence: true 
   validates :title, presence: true 
