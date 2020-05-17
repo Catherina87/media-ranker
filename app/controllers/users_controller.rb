b020f1eb584eef277b45f471816232a42927fbc5
Catherina <catherine.pchelina@gmail.com>
2020-05-16 19:56:33 -0700

    Add login_form and login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def login_form 
+    @user = User.new
+  end
+
+  def login 
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+
+    if user 
+      session[:user_id] = user.id 
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      new_user = User.create(username: username)
+      session[:user_id] = new_user.id 
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+
+    redirect_to root_path
+    return
+  end
+
 end
