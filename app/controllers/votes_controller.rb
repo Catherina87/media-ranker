045029e4c9f1545353a4e45be534ea9b1afee0b3
Catherina <catherine.pchelina@gmail.com>
2020-05-18 16:06:01 -0700

    Add vote method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,29 @@
 class VotesController < ApplicationController
 
+
+  def create 
+    
+    if session[:user_id] == nil 
+      flash[:error] = "Please log in to upvote"
+      return
+    end
+
+    maybe_vote = Vote.find_by(
+      work_id: params[:work_id], 
+      user_id: session[:user_id]
+    )
+
+    if maybe_vote == nil
+      @vote = Vote.create(
+        work_id: params[:work_id],
+        user_id: session[:user_id],
+        date: "TODAY"
+      )
+    else 
+      flash[:error] = "Already voted"
+      return
+    end
+
+    redirect_to work_path(params[:work_id])
+  end
 end
