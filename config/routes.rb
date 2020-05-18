46c7380c6f3102689bf910550f124e2da03f6890
Catherina <catherine.pchelina@gmail.com>
2020-05-17 18:43:47 -0700

    Update routes and add vote controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user" 
+
+  resources :users, only: [:index, :show]
+
+  resources :works do 
+    resources :votes, only: [:create]
+  end
 end
