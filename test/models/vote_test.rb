d2c8595448552efe681077a8afaa85c9fbdb21a9
Catherina <catherine.pchelina@gmail.com>
2020-05-19 14:46:57 -0700

    Add tests for Vote model

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do 
+    it "can access work through work_id" do 
+      vote = Vote.create(
+        work_id: works(:titanic).id,
+        user_id: users(:user_one).id,
+        date: Date.today
+      )
+
+      expect(vote.work_id).must_equal works(:titanic).id
+    end
+
+    it "can access work through work" do 
+      vote = Vote.create(
+        work_id: works(:titanic).id,
+        user_id: users(:user_one).id,
+        date: Date.today
+      )
+
+      expect(vote.work.title).must_equal works(:titanic).title
+    end
+
+    it "can access usre through user_id" do 
+      vote = Vote.create(
+        work_id: works(:titanic).id,
+        user_id: users(:user_one).id,
+        date: Date.today
+      )
+
+      expect(vote.user_id).must_equal users(:user_one).id
+    end
+
+    it "can access user through user" do 
+      vote = Vote.create(
+        work_id: works(:titanic).id,
+        user_id: users(:user_one).id,
+        date: Date.today
+      )
+
+      expect(vote.user.username).must_equal users(:user_one).username
+    end
+  end
 end
