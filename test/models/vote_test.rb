af28b3f977a5863ed052ae695c9c678c4bf9f4c0
Catherina <catherine.pchelina@gmail.com>
2020-05-21 19:29:24 -0700

    Add vote validation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -42,4 +42,20 @@ describe Vote do
       expect(vote.user.username).must_equal users(:user_one).username
     end
   end
+
+  describe "validations" do 
+    it "must have a user id" do
+      vote = Vote.create(work_id: works(:titanic).id, date: Date.today)
+
+      expect(vote.valid?).must_equal false
+      expect(vote.errors.messages).must_include :user
+    end
+
+    it "must have a work id" do 
+      vote = Vote.create(user_id: users(:user_one).id, date: Date.today)
+
+      expect(vote.valid?).must_equal false
+      expect(vote.errors.messages).must_include :work
+    end
+  end
 end
